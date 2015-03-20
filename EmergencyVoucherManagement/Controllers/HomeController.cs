042c5885e22151a9ccfe997ad6c64cec18b6b1ee
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-03-20 15:23:09 -0400

    Template Report

--- EmergencyVoucherManagement/Controllers/HomeController.cs
+++ EmergencyVoucherManagement/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
+using RazorEngine;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
@@ -11,6 +13,8 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using TuesPechkin;
+using RazorEngine.Templating;
+
 
 namespace EmergencyVoucherManagement.Controllers
 {
@@ -21,21 +25,52 @@ namespace EmergencyVoucherManagement.Controllers
             return View(); // Redirect("~/App/");
         }
 
-        public ActionResult TestReport()
+        public ActionResult TestReport(int vendorId, int distributionId, string period)
         {
-            var loopbackURL = ConfigurationManager.AppSettings["Loopback.URL"];
-
             var content = new StringBuilder();
-            content.Append("<html>");
-            content.Append("<head>");
-            content.Append("<style>");
-            content.Append("body { color: Red; }");
-            content.Append("</style>");
-            content.Append("</head>");
-            content.Append("<body>");
-            content.AppendFormat("<img src=\"{0}Content/IRCLogo.svg\"/><h1>IS this red?</h1>{0}Content/IRCLogo.svg", loopbackURL);
-            content.Append("</body>");
-            content.Append("</html>");
+            var report = System.IO.File.ReadAllText(Server.MapPath("~/Reports/VendorReceiptReport.cshtml"));
+            var image = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/IRCLogo.svg"));
+            var periodDate = DateTime.Parse(period);
+            using (var ctx = new Models.Vouchers.Context())
+            {
+                var items = (from v in ctx.VoucherTransactionRecords
+                            where
+                            v.VendorId == vendorId && v.Voucher.DistributionId == distributionId
+                            orderby v.Beneficiary.Name
+                            select v).ToArray();
+
+                items = Enumerable.Repeat(items.First(), 35).ToArray();
+                int pageSize = 12;
+                var pages = new List<dynamic>();
+                var numberOfPages = (int)Math.Ceiling(items.Length / (double)pageSize);
+                for (int i = 0; i < numberOfPages; i++) {
+                    pages.Add(new
+                    {
+                        Page = i + 1,
+                        IsLastPage = (i + 1) == numberOfPages,
+                        Items = items.Skip(i * pageSize).Take(pageSize).ToArray(),
+                        SubTotal = items.Skip(i * pageSize).Take(pageSize).Select(v => v.Voucher.Value).Sum()
+                    });
+                }
+
+                var model = new
+                {
+                    User = User.Identity,
+                    Distribution = ctx.Distributions.Where(d=> d.Id==distributionId).First(),
+                    Period = periodDate,
+                    Pages = pages,
+                    Items = items,
+                    PageSize = pageSize,
+                    Vendor = ctx.Vendors.Where(v => v.Id == vendorId).First(),
+                    Logo = Convert.ToBase64String(image),
+                    LogoMimeType = "image/svg+xml",
+                    Total = items.Select(i=>i.Voucher.Value).Sum()
+                };
+                var compiled = Engine.Razor.RunCompile(report, Guid.NewGuid().ToString(), null, model);
+
+                content.Append(compiled);
+
+            }
 
             var document = new HtmlToPdfDocument
             {
@@ -46,9 +81,10 @@ namespace EmergencyVoucherManagement.Controllers
                     PaperSize = PaperKind.A4, // Implicit conversion to PechkinPaperSize
                     Margins =
                     {
-                        All = 1.375,
+                        All = 1,
                         Unit = Unit.Centimeters
-                    }
+                    },
+                    Orientation = TuesPechkin.GlobalSettings.PaperOrientation.Landscape
                 },
                 Objects = {
                     new ObjectSettings { HtmlText = content.ToString() },
