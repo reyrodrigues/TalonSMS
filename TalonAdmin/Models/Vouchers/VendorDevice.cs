ab7d7744258b8d0ebbfa622d9c64dd0de8103583
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-05-14 17:19:42 -0400

    Committing WIP

---
+++ TalonAdmin/Models/Vouchers/VendorDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalonAdmin.Models.Vouchers
+{
+    public class VendorDevice : CountryEntity
+    {
+        public virtual int VendorId { get; set; }
+        public virtual string UUID { get; set; }
+        public virtual string Type { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string IPAddress { get; set; }
+        public virtual DateTime LastAccessed { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+    }
+}
