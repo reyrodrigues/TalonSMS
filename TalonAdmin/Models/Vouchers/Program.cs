697fdd6c875da46745898930cf8a947efdef179f
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-06-17 12:17:47 -0400

    Hotfix Dashboard

--- TalonAdmin/Models/Vouchers/Program.cs
+++ TalonAdmin/Models/Vouchers/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace TalonAdmin.Models.Vouchers
 {
@@ -11,23 +13,8 @@ namespace TalonAdmin.Models.Vouchers
         public string Name { get; set; }
         public string FundCodes { get; set; }
 
-        /// <summary>
-        /// 1. SMS
-        /// 2. NFC Card Load
-        /// 3. QR Code - Paper
-        /// </summary>
-        public virtual int DistributionMechanism { get; set; }
-
-        [DefaultValue(6)]
-        public virtual int VoucherCodeLength { get; set; }
-
-        public virtual string CreatedBy { get; set; }
-        public virtual string ModifiedBy { get; set; }
-        public virtual DateTime CreatedOn { get; set; }
-        public virtual DateTime ModifiedOn { get; set; }
-
+        [JsonIgnore, XmlIgnore, IgnoreDataMember]
         public virtual ICollection<Distribution> Distributions { get; set; }
-        public virtual ICollection<ProgramVoucherCategory> Categories { get; set; }
-
+        
     }
 }
