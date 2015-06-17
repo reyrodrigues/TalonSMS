08f0423f260bf547bef5d81cf09d521eb5f46055
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-06-17 12:25:35 -0400

    Third time is the charm

--- TalonAdmin/Models/Vouchers/Program.cs
+++ TalonAdmin/Models/Vouchers/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace TalonAdmin.Models.Vouchers
 {
@@ -9,5 +12,9 @@ namespace TalonAdmin.Models.Vouchers
     {
         public string Name { get; set; }
         public string FundCodes { get; set; }
+
+        [JsonIgnore, XmlIgnore, IgnoreDataMember]
+        public virtual ICollection<Distribution> Distributions { get; set; }
+        
     }
 }
